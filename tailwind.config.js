a5c85419def01bf498220bd9fdf6c56aa3fbd4f2
Adnan Haider <118106335+AdnanMehr8@users.noreply.github.com>
2024-07-03 17:03:40 +0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  // content: [
+  //   "./src/**/*.{js,jsx,ts,tsx}",
+  // ],
+   purge: {
+    content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+    safelist: ['animate-autoBlur', 'animate-autoShow'],
+  },
   theme: {
     extend: {
       colors: {
