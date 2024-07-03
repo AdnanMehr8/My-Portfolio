8e185dbeb530d623dee7edebec0e53e24d174810
Adnan Haider <118106335+AdnanMehr8@users.noreply.github.com>
2024-07-03 18:46:01 +0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+    mode: 'jit',
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
   // content: [
   //   "./src/**/*.{js,jsx,ts,tsx}",
-  // ],
-   purge: {
-    content: ['./src/**/*.{js,jsx,ts,tsx}', './public/manifest.json'],
-    safelist: ['autoBlur', 'autoShow'],
+  // // ],
+  //  purge: {
+  //   content: ['./src/**/*.{js,jsx,ts,tsx}', './public/manifest.json'],
+  //   safelist: ['autoBlur', 'autoShow'],
   },
   theme: {
     extend: {
